cd73735d77f133977eb0502e09d44cf78c84b956
hirokam <h.kameyama0329@gmail.com>
2023-11-22 14:10:35 +0900

    ブラウザ表示

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function contact() {
+        return view('contact');
+    }
+
+    public function confirm() {
+        return view('confirm');
+    }
+
+    public function thanks() {
+        return view('thanks');
+    }
+}
